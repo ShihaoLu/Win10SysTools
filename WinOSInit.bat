1dc1cf244c3a71332eed7661b5655bd6a6f6351b
LIU Shihao <liu.shihao@hotmail.com>
2025-08-24 17:27:53 +0800

    WinOSInit.bat

---
+++ WinOSInit.bat
@@ -0,0 +1,37 @@
+::
+::
+
+
+SET APPNAME=WinOSInit
+SET VERSION=0.1.0
+SET AUTHOR=LIU Shihao [liu.shihao@hotmail.com]
+SET MODIFY=2025/08/24
+
+
+@ECHO OFF
+::ShowUsage
+ECHO ===================================================
+ECHO    AppName    %APPNAME%
+ECHO    Version    %VERSION%
+@ECHO.
+ECHO    Author     %AUTHOR%
+ECHO    Modified   %MODIFY%
+ECHO ===================================================
+@ECHO.
+@ECHO.
+
+
+::获取系统版本信息
+ECHO Get OS version... & ver
+@ECHO.
+
+
+
+::交换LeftCtrl按键 和 CapsLock按键
+ECHO 01. To remap LeftCtrl key and CapsLock key...
+regedit /s .\tools\Ctrl2CapsLock.reg
+If %errorlevel% NEQ 0 (
+    ECHO [Error] Failed to remap LeftCtrl and CapsLock keys!
+) ELSE (
+    ECHO %tab%Succeed to remap LeftCtrl and CapsLock keys.
+)
--- tools/Win10.ps1
+++
@@ -1,8 +0,0 @@
-##########
-# Win 10 Initial Setup Script - Main execution loop
-# Author: Liu Shihao <liu.shihao@hotmail.com>
-# Version: v0.1, 2021-01-12
-##########
-
-
-Get-Help
